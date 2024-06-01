a62a8218f379105e006e3eaaef0f7793bdc93f8a
Rohan Ghorpade <64244858+RohanG2001@users.noreply.github.com>
2024-06-01 18:01:44 +0530

    removed preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-
-document.addEventListener('DOMContentLoaded', () => {
-  "use strict";
-
-
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
-  }
-
   /**
    * Mobile nav toggle
    */
